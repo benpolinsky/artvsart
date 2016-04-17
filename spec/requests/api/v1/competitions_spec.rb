edd921d7fd8f523a318f887b6a2ca82c922641ef
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-04-16 20:06:50 -0400

    Updating Specs

--- spec/requests/api/v1/competitions_spec.rb
+++ spec/requests/api/v1/competitions_spec.rb
@@ -1,29 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "Competitions API" do
-  before do
-    create_list(:art, 2)
-    get '/api/v1/battle'
-  end
+  context '/battle' do
+    before do
+      create_list(:art, 2)
+      get '/api/v1/battle'
+    end
   
-  it "returns ok" do
-    expect(response.code).to eq "200"
-  end
+    it "returns ok" do
+      expect(response.code).to eq "200"
+    end
   
-  it "returns necessary attributes for a new competition" do
-    COMPETITION_KEYS_HIDDEN = ["updated_at", "created_at", "winner"]
-    json = JSON.parse(response.body)
-    # add additional content when up to it
-    expect(json["competition"]["id"]).to_not be_nil    
-    expect(json["competition"]["art"]["name"]).to_not be_nil
-    expect(json["competition"]["challenger"]["name"]).to_not be_nil
+    it "returns necessary attributes for a new competition" do
+      COMPETITION_KEYS_HIDDEN = ["updated_at", "created_at", "winner"]
+      json = JSON.parse(response.body)
+      # add additional content when up to it
+      expect(json["competition"]["id"]).to_not be_nil    
+      expect(json["competition"]["art"]["name"]).to_not be_nil
+      expect(json["competition"]["challenger"]["name"]).to_not be_nil
   
-    COMPETITION_KEYS_HIDDEN.each do |comp_key|
-      expect(json["competition"][comp_key]).to be_nil
+      COMPETITION_KEYS_HIDDEN.each do |comp_key|
+        expect(json["competition"][comp_key]).to be_nil
+      end
     end
   end
   
-  it "can choose an art to win the battle" do
+  context '/pick_winner' do
+    before do
+      create(:competition)
+    end
     
+    it "can choose an art to win the battle" do
+
+    end
   end
 end
