505f37adf6505b75c156ea39f8b0376297dc609e
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-10-28 17:40:17 -0400

    update facebook uptions

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :email, :auth_token, :type, :gravatar_hash
+  attributes :email, :auth_token, :type, :gravatar_hash, :identities
   
   def type
     object.admin? ? 'admin' : object.type
+  end
+  
+  def identities
+    object.identities && object.identities.map(&:provider)
   end
 end
